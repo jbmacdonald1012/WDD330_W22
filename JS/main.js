444730301381c835e0effcc0d16ed770686471f7
jbmacdonald1012 <jbmacdonald1012@gmail.com>
2022-01-15 21:17:27 -0700

    rework main.js for fresh start

--- JS/main.js
+++
@@ -1,9 +0,0 @@
-function tableOfContents(){
-    const links = [
-    {
-        label: "Week 1 notes",
-        url: "week1/index.html"
-    }
-]
-    document.getElementById("table-of-contents").innerHTML=links;
-}
